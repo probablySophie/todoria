40d9e52cd2f9445e939e34ec2695509707eddc00
ProbablySophie <70588905+probablySophie@users.noreply.github.com>
2024-10-03 15:42:04 +0800

    yay, a simple main file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+mod app;
+mod test;
+
+const TITLE: &str = "TODOria";
+const DEBUG: bool = true;
+
+// https://ratatui.rs/tutorials/counter-app/basic-app/#displaying-the-application
+// In app.rs
+
+fn main() -> io::Result<()>
+{
+	let mut terminal = ratatui::init();
+	terminal.clear()?;
+	let app_result = app::App::default().run(terminal);
+	ratatui::restore();
+	if DEBUG { println!("Quit Safely :)") }
+	app_result
 }
