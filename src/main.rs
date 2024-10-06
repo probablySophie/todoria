27cbe33afb41e198aa2cddf0fd7413ce8762d39b
ProbablySophie <70588905+probablySophie@users.noreply.github.com>
2024-10-06 13:38:28 +0800

    moved render

--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,39 @@ const DEBUG: bool = true;
 fn main() -> io::Result<()>
 {	
 	let mut terminal = ratatui::init();
+	
 	terminal.clear()?;
+	
 	let app_result = app::App::default().run(terminal);
+	
 	ratatui::restore();
-	if DEBUG { println!("Quit Safely :)") }
-	app_result
+
+	// If DEBUG, print our debug string :)
+	if DEBUG
+	{
+		if let Ok(result) = &app_result
+		{
+			println!("Quit Safely :)");
+			println!("Debug info: \n\x1b[93m{}\x1b[0m", result.clone());
+		}
+		else
+		{
+			println!("Did not exit safely!!!");
+		}
+	}
+
+	// And return an io::Result<()>
+	if app_result.is_ok()
+	{
+		Ok(())
+	}
+	else
+	{
+		Err(
+			io::Error::new(
+				io::ErrorKind::Other,
+				"App did not exit successfully"
+			)
+		)
+	}
 }
