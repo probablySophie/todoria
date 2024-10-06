38c651952ca3ecc56925bf072ad695157da7c612
ProbablySophie <70588905+probablySophie@users.noreply.github.com>
2024-10-06 13:37:19 +0800

    moved change_state

--- src/app/state.rs
+++ src/app/state.rs
@@ -1,4 +1,7 @@
 use std::io;
+use ratatui::prelude::Stylize;
+
+use crate::app:: {keybinds::char_from_action, Action};
 
 #[derive(Debug, PartialEq, Clone, Copy)]
 pub enum State
@@ -32,5 +35,37 @@ impl State
             ),
             
         }
+    }
+}
+
+impl crate::app::App
+{
+    
+    /// Update the app's `self.state`, also sets `self.previous_state`
+    pub fn change_state(&mut self, state: State)
+    {
+        self.previous_state = self.state;
+        self.state = state;
+
+        self.instructions = ratatui::text::Line::from(match state
+        {
+            State::Main => {
+                vec![
+                    
+                    " Quit: ".into(), 
+                    char_from_action(&self.keybinds, Action::Quit, State::Main).blue().bold(),
+                    " Select: ".into(),
+                    char_from_action(&self.keybinds, Action::Select, State::Main).blue().bold(),
+                    " ".into(),
+                ]            
+            },
+            
+            State::Focused
+            | State::UnsavedChanges
+            | State::Settings
+            | State::Filter
+            | State::Sort
+            | State::All => { vec![ "No Instructions Yet :(".red().bold() ] }
+        });        
     }
 }
