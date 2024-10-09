c76fee31f0265e0df440488113a6778ef0ffe8d9
ProbablySophie <70588905+probablySophie@users.noreply.github.com>
2024-10-09 17:23:28 +0800

    plans

--- src/app/render/settings.rs
+++ src/app/render/settings.rs
@@ -10,7 +10,10 @@
 
 /*
 	TODO: Make a popup item that appears with a list of valid options for the settings
-	TODO: Make a popup item with an input text box for fuzzy searching
-	TODO: A calendar popup for selecting sorting dates?
+	      * A list of options from which you can select one (e.g. whether to insert creation-date by default)
+	      * A list of options from which you can toggle multiple (e.g. rows to show)
+	      * An input text box
+	      
+	TODO: A calendar popup for selecting sorting dates? (and date ranges)
 */
 
